0a028f3bbf7a50234ee52261bd6a52946509bb24
Gabriel Walter Ysidro Palomino <gawayspa@gmail.com>
2024-11-18 20:44:57 -0500

    Setup del GUI

--- GUI/GUI.cpp
+++ GUI/GUI.cpp
@@ -1,6 +1,31 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-int main () {
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Error al inicializar GLFW" << std::endl;
+        return -1;
+    }
 
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Ventana GLFW", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Error al crear la ventana GL" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Tu código de renderizado aquí...
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
